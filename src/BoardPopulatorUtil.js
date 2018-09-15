403f915bd5d201a22424701e23087a3319d7adb8
Philipp Blume <pgbluster@gmail.com>
2018-09-15 17:05:34 -0500

    refactor word selector

--- src/BoardPopulatorUtil.js
+++ src/BoardPopulatorUtil.js
@@ -8,16 +8,14 @@ export default function BoardPopulator(...dimensions) {
   const number_of_rows = dimensions[0];
   const number_of_tiles_per_row = dimensions[1];
   const total_tiles = dimensions.reduce((acc,curr) => acc * curr)
-  let sample = [];
   let sample_text = loremIpsum({
     count: 15,
     units: 'paragraphs',
     format: 'plain'
   });
-  sample = sample_text.replace(/[A-Z,. ]/g,'');
+  let sample = sample_text.replace(/[A-Z,. ]/g,'').replace(/qu/g,'q');
   let start = sample.indexOf('e');
-  sample = sample.substr(start, start + (total_tiles)).split("");
-  sample = shuffle(sample);
+  sample = shuffle(sample.substr(start, start + (total_tiles)).split("").map(x => x.toUpperCase()));
   let result = []
   for (let i=0; i < number_of_rows; i++) {
     result.push([]);
